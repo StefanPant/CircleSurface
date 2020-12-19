84f58b81ca23375da5a4a589de5d0daa4c7eaed1
StefanPant <42117058+StefanPant@users.noreply.github.com>
2020-12-19 21:39:47 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krug
+{
+    delegate double Surface(double r);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Surface Equals = (r) =>
+            {
+                return r * r * Math.PI;
+                
+            };
+
+            Console.WriteLine("Result is: {0}", Equals(5));
+
+
+            Console.ReadKey();
+        }
+    }
+
+
+
+}
